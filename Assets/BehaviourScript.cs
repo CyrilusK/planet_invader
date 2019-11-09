18986b4c4747bb848d0064ce3c17996549abf503
CyrilusK <kardash199696@mail.ru>
2019-11-09 23:11:24 +0300

    1st commit

---
+++ Assets/BehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourScript : MonoBehaviour
+{
+    public int population;
+    public int population_of_user;
+    public int population_of_bot;
+    public bool planet_of_user;
+    public bool planet_of_bot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
