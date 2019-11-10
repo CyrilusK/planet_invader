f3e10332a1fbf1d7a879f211f55224063994c965
Cyril Kardash <kardash199696@mail.ru>
2019-11-10 16:31:56 +0300

    прирост населения на планетах игрока и бота

--- Assets/BehaviourScript.cs
+++ Assets/BehaviourScript.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class BehaviourScript : MonoBehaviour
 {
     public int population;
-    public int population_of_user;
-    public int population_of_bot;
-    public bool planet_of_user;
-    public bool planet_of_bot;
+    public bool owned_by_user;
+    public bool owned_by_bot;
+    public bool neutral;
+    public float counter;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ((owned_by_user == true) || (owned_by_bot == true)) {
+            counter += Time.deltaTime;
+            if (counter >= 1)
+            {
+                population += 1;
+                counter = 0;
+            }
+        }
     }
 }
